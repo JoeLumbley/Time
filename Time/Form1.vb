1c8100822b505a70c0840846380695fd870b606a
Joseph Lumbley <joelumbley16@gmail.com>
2024-11-14 00:23:55 -0600

    Clean up

--- Time/Form1.vb
+++ Time/Form1.vb
@@ -1,5 +1,14 @@
 ﻿' Time🕒
 
+' A simple Windows application that displays the current local time.
+' The application utilizes optimized rendering techniques to ensure smooth
+' updates and an aesthetically pleasing user interface.
+
+' Features
+'   Real-time display of the current time.
+'   Responsive design that adjusts font size and position based on the window size.
+'   Utilizes buffered graphics for improved performance and visual quality.
+
 ' MIT License
 ' Copyright(c) 2024 Joseph W. Lumbley
 
